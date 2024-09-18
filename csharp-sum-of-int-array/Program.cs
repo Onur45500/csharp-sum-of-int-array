aa1adc4e57961d9be25705d3227d77b63d9ec818
Onur45500 <onur.akmese@viacesi.fr>
2024-09-18 22:11:02 +0200

    Created two function static void functions, one simple the other using ref

--- csharp-sum-of-int-array/Program.cs
+++ csharp-sum-of-int-array/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
+            int[] array = new int[10]
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
 
+            CalcSumArray(array);
+
+
+            int sum = 0;
+            CalcSumArrayByRef(array, ref sum);
+            Console.WriteLine($"The sum of the array is {sum} by ref");
+        }
+
+        static void CalcSumArray(int[] pArray)
+        {
+            int sum = 0;
+            for(int i = 0; i <= pArray.Length - 1; i++)
+            {
+                sum = sum + pArray[i];
+            }
+
+            Console.WriteLine($"The sum of the array is {sum}");
+        }
+
+        static void CalcSumArrayByRef(int[] pArray, ref int pSum)
+        {
+            for (int i = 0; i <= pArray.Length - 1; i++)
+            {
+                pSum = pSum + pArray[i];
+            }
         }
     }
 }
